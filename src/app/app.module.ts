f73d35be1747d3a7452d64d4c9bffbfe6b0a0069
samiksha <samimarahatta@gmail.com>
2022-01-19 14:25:56 +0545

    added kendo ui label,form fileds, button packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { ButtonComponent } from './shared/button/button.component';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LabelModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    InputsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
